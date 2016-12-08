9482bad87f9a5828c820b05712bf8aad1c4f7d1b
nickbuker <nickbuker@gmail.com>
2016-12-07 16:33:33 -0800

    add percent

--- select_from_select5.sql
+++ select_from_select5.sql
@@ -1,6 +1,6 @@
-SELECT name, ROUND(100*(population/
+SELECT name, CONCAT(ROUND(100*(population/
   (SELECT population
     FROM world
-    WHERE name = 'Germany')), 0) as percentage
+    WHERE name = 'Germany')), 0), '%') as percentage
   FROM world
   WHERE continent = 'Europe';
