1b53dba9cbb244f3b249b3b050dbda616941979a
nickbuker <nickbuker@gmail.com>
2016-12-18 10:02:01 -0800

    more self join practice

---
+++ self_join/self_join9.sql
@@ -0,0 +1,18 @@
+SELECT DISTINCT
+  stopb.name
+  ,a.company
+  ,a.num
+FROM
+  route a JOIN route b
+ON
+  (a.company = b.company AND a.num = b.num)
+  JOIN stops stopa
+    ON a.stop = stopa.id
+  JOIN stops stopb
+    ON b.stop = stopb.id
+WHERE
+  a.company = 'LRT'
+  AND
+  stopa.name = 'Craiglockhart'
+ORDER BY
+  stopb.name;
