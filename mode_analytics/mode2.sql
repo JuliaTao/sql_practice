532958ac30c28ba129a260bb94968579f1402398
nickbuker <nickbuker@gmail.com>
2017-02-06 11:05:43 -0800

    add sql practice

---
+++ mode_analytics/mode2.sql
@@ -0,0 +1,10 @@
+SELECT
+  year AS Year
+  ,month AS Month
+  ,month_name AS Month_Name
+  ,south AS South
+  ,west AS West
+  ,midwest AS Midwest
+  ,northeast AS Northeast
+FROM
+  tutorial.us_housing_units
